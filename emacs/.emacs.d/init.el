b6be9d254badb85cfa03df3531320f70772a78bf
XLDou <douxlghost@gmail.com>
2024-12-08 19:09:40 -0500

    nvim and emacs init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,8 +2,10 @@
 
 (require' package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
 (package-refresh-contents)
@@ -18,6 +20,80 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+(use-package tex
+  :ensure auctex
+  :config
+
+  ;; Indent items by two spaces.
+  (setq LaTeX-item-indent 0)
+  
+  (setq TeX-PDF-mode t)
+
+  ;; Generate sync file and sync with C-v
+  (eval-after-load
+      "tex" '(add-to-list 'TeX-command-list
+                          '("latexmk" "latexmk -pdf %t --synctex=1" TeX-run-TeX)))
+
+  (setq latex-run-command "pdflatex")
+  (setq LaTeX-command "latex --synctex=1")
+
+  ;; Use pdf-tools to open PDF files
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+        TeX-source-correlate-start-server t
+        TeX-source-correlate-method 'synctex)
+
+  (setq TeX-auto-save t
+        TeX-parse-self t)
+
+  ;; Needed to sync TeX and PDF
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (TeX-source-correlate-mode 1)))
+
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer)
+
+  ;; (add-hook 'pdf-view-mode-hook 'auto-revert-mode)
+  ;; (setq auto-revert-interval 0.5)
+
+  (add-hook 'pdf-view-mode-hook
+            (lambda ()
+              (pdf-view-fit-page-to-window) ))
+
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (reftex-mode))))
+
+
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
 ;; org-mode
 (use-package org-bullets
   :ensure t
@@ -28,11 +104,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-bullets which-key try)))
+ '(package-selected-packages
+   '(0blayout which-key try pdf-tools org-bullets gruvbox-theme auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'upcase-region 'disabled nil)
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,21 @@
+vim.cmd("syntax enable")
+vim.cmd("filetype plugin on")
+vim.cmd("set path +=**")
+
+vim.cmd("set wildmenu")
+
+vim.cmd("syntax on")
+vim.cmd("set number")
+vim.cmd("set cursorline")
+vim.cmd(":highlight Cursorline cterm=bold ctermbg=black")
+vim.cmd("")
+vim.cmd("set hlsearch")
+
+vim.cmd("set tabstop     =4")
+vim.cmd("set softtabstop =4")
+vim.cmd("set shiftwidth  =4")
+vim.cmd("set textwidth   =79")
+vim.cmd("set expandtab")
+vim.cmd("set autoindent")
+vim.cmd("set showmatch")
+vim.cmd("map <F6> :setlocal spell! spelllang=en_us<CR>")
